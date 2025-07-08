6dfda17fd9a338813b91fa8894064ff25995f626
danielryz <danielryz4321@gmail.com>
2025-07-08 23:41:36 +0200

    Add OpenAPI documentation

---
+++ weddinggallery/src/main/java/com/weddinggallery/config/OpenApiConfig.java
@@ -0,0 +1,16 @@
+package com.weddinggallery.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Wedding Gallery API",
+                version = "1.0",
+                description = "API documentation for Wedding Gallery"
+        )
+)
+public class OpenApiConfig {
+}
