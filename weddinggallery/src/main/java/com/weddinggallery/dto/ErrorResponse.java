ac35691232c20f9e6db6c1652a04eb8948900fd2
danielryz <danielryz4321@gmail.com>
2025-07-09 17:55:25 +0200

    Add global exception handling

---
+++ weddinggallery/src/main/java/com/weddinggallery/dto/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.weddinggallery.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+}
