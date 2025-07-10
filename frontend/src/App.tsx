1b5dc5a21133394a9c3b90b8c751b2585634a6d0
danielryz <danielryz4321@gmail.com>
2025-07-10 21:30:14 +0200

    Add routing with placeholder pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
 import { login } from './api/auth'
+import Home from './pages/Home'
+import GalleryPage from './pages/GalleryPage'
+import PhotoDetailPage from './pages/PhotoDetailPage'
+import ChatPage from './pages/ChatPage'
+import MenuPage from './pages/MenuPage'
 
 function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [name, setName] = useState('')
+  const [isAuth, setIsAuth] = useState(() => Boolean(localStorage.getItem('token')))
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -12,31 +19,44 @@ function App() {
     setUsername('')
     setPassword('')
     setName('')
+    setIsAuth(true)
+  }
+
+  if (!isAuth) {
+    return (
+      <div>
+        <h1>Wedding Gallery</h1>
+        <form onSubmit={handleLogin}>
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+          />
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Device Name"
+          />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
   }
 
   return (
-    <div>
-      <h1>Wedding Gallery</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="Device Name"
-        />
-        <button type="submit">Login</button>
-      </form>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/gallery" element={<GalleryPage />} />
+      <Route path="/photos/:id" element={<PhotoDetailPage />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/menu" element={<MenuPage />} />
+    </Routes>
   )
 }
 
---
+++ frontend/src/pages/PhotoDetailPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function PhotoDetailPage() {
+  return <div>Photo Detail Page</div>
+}
+
+export default PhotoDetailPage
