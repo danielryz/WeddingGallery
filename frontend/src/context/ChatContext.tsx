62200b99d97e9691ebecefd2f1c5118d32dbf936
danielryz <danielryz4321@gmail.com>
2025-07-10 22:05:19 +0200

    Implement STOMP chat in ChatPage

--- frontend/src/context/ChatContext.tsx
+++ frontend/src/context/ChatContext.tsx
@@ -52,6 +52,7 @@ export function ChatProvider({ children }: { children: React.ReactNode }) {
   )
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useChat() {
   const ctx = useContext(ChatContext)
   if (!ctx) throw new Error('useChat must be used within ChatProvider')
